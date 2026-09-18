67b7011a6478b8756300e2cc6a872c83b5115f71
Rodrigo Rocha <rodrigorochaed1@gmail.com>
2026-09-17 22:48:06 -0300

    feat: add Pessoa and Funcionario model classes

--- src/main/java/com/prothera/teste/Principal.java
+++ src/main/java/com/prothera/teste/Principal.java
@@ -1,8 +1,12 @@
 package com.prothera.teste;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("Projeto inicializado.");
+        Funcionario funcionario = new Funcionario("Maria", LocalDate.of(2000, 10, 18), new BigDecimal("2009.44"), "Operador");
+        System.out.println(funcionario);
     }
 }
